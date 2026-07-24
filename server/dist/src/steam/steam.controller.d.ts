c2e00f338e34d0c039b84d3569072304e3fbddc5
ArseniBogatorjov <144094579+ArseniBogatorjov@users.noreply.github.com>
2026-07-24 18:16:38 +0300

    chore: fixed gitignore paths

--- server/dist/src/steam/steam.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { SteamService } from './steam.service';
-export declare class SteamController {
-    private readonly steamService;
-    constructor(steamService: SteamService);
-    fetchUserGames(steamId: string): Promise<import("./steam.service").SteamGetOwnedGamesResponse>;
-}
